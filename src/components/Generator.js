e4bef8239d42098464db4a86f171e97120d5835d
Rene Rodriguez <rene.rrrg@gmail.com>
2020-05-04 20:03:41 +0200

    Doing CSS

--- src/components/Generator.js
+++ src/components/Generator.js
@@ -2,53 +2,60 @@ import React from "react";
 import ShowMessage from "./ShowMessage"
 
 class Generator extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-this.state = {
-    addVal: [],
-    val: []
+        this.state = {
+            addVal: [],
+            val: '',
+            plug: false
+        }
+
+        console.log(this.state)
+        this.changeText = this.changeText.bind(this);
+        this.addValue = this.addValue.bind(this);
+        this.show = this.show.bind(this);
     }
 
-console.log(this.state)
-this.changeText = this.changeText.bind(this);
-this.addValue = this.addValue.bind(this);
-}
+    changeText(evt) {
+        console.log(evt.target.value)
+        this.setState({ val: evt.target.value })
+    }
 
-changeText(evt) {
-console.log(evt.target.value)
-this.setState({val: [...evt.target.value].join("")})
-}
+    addValue(evt) {
+        // let peoplArrayClone = [...this.state.val];
+        this.setState(previousState => ({
+            addVal: [...previousState.addVal, this.state.val],
+            val: []
+        }))
+        console.log(this.state)
+        evt.preventDefault();
+    }
+
+    show(e) {
+        e.preventDefault();
+        if (this.state.addVal.length >= 2) {
+            this.setState({ plug: true })
+        }
+    }
 
 
-addValue(evt) {
-    // let peoplArrayClone = [...this.state.val];
- this.setState(previousState => ({
-     addVal: [...previousState.addVal, [...this.state.val].join("")],
-     val: []
- }))
- 
-   console.log(this.state)
-   evt.preventDefault();
-}
-
-
-    render(){
-        return(
+    render() {
+        return (
             <div>
-                {
-                <ShowMessage words={this.state.addVal}/>
-    }
+                {this.state.plug &&
+                    <ShowMessage words={this.state.addVal} />
+                }
                 <form>
                     <label htmlFor="text">Add words
-        <input type="text" value={this.state.val} onChange={evt => this.changeText(evt)} name={this.state.val}/>
+        <input type="text" value={this.state.val} onChange={evt => this.changeText(evt)} name={this.state.val} />
                     </label>
                     <label>
-                        <button >Show message</button>
+                        <button onClick={this.show}>Show message</button>
                         <button onClick={this.addValue}>Add value</button>
                     </label>
                 </form>
                 <div>
-        <p>Im a {this.state.val} updated {this.state.addVal}</p>
+                    <p>Im a {this.state.val} updated {this.state.addVal}</p>
                 </div>
             </div>
         )
--- src/components/ShowMessage.js
+++ src/components/ShowMessage.js
@@ -5,44 +5,32 @@ class ShowMessage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            word: []
+            counter: 0
         }
-        console.log(this.state.word)
+
         this.wordsFunc = this.wordsFunc.bind(this);
         this.counter = 0;
-        this.fireFunction = this.fireFunction.bind(this);
     }
 
-    fireFunction() {
-       
-        this.setState(previousState => ({
-            word: [...previousState.word, [...this.props.words]]
-        }))
-    
-        console.log('here!' + this.props.words)
-}
-
-    componentWillMount() {
+    componentDidMount() {
         // if (this.props.words.length >= 2) {
         //     this.setState({ word: [this.state.word, [...this.props.words]] })
         // }
         this.intervalId = setInterval(this.wordsFunc, 2000);
-       
+
     }
 
-    componentDidMount(){
-        if(this.props.words > 0){
-            this.fireFunction();
-        }
-    }
+   
 
     wordsFunc() {
-        if (this.counter < 2) {
-            this.counter++
-        } else { this.counter = 0 }
-        console.log(this.counter)
-        console.log(this.state.word)
-      
+        this.setState((state) => {
+            const newCounter = this.props.words.length - 1 == this.state.counter ? 0 : state.counter++
+            return { 
+                counter: newCounter
+             }
+        })
+
+
     }
 
     componentWillUnmount() {
@@ -53,7 +41,7 @@ class ShowMessage extends React.Component {
 
         return (
             <div>
-                <p >I'm a {this.state.word}</p>
+                <p >I'm a {this.props.words[this.state.counter]}</p>
             </div>
         )
     }
